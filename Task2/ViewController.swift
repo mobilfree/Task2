8fea71e93f7d6aa044d5d55b0dbf24012c683709
Artem Shchegolev <mobilfree@mail.ru>
2021-08-18 16:48:30 +0300

    Initial Commit

---
+++ Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by Artem on 18.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
